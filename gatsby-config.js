4d998013d898d69e066b13741bbf52ef87910a06
Aaron <aaron.perrin@gmail.com>
2019-12-02 10:55:29 -0500

    updated description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Usguyz Podcast - Home`,
+    description: `Experimental, long-form, discussion podcast made with balls in Northern Appalachia`,
+    author: `@usguyzpodcast`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
