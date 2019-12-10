34fe69d785df18cc9eefc21481540379ccbb6006
Aaron <aaron.perrin@gmail.com>
2019-12-09 20:25:38 -0500

    analytics issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,19 +67,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-154334574-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "usguyzpod.com",
+        trackingId: "UA-154334574-1"
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
