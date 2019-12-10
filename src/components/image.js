e79818531626bda53da2eda66a889aa9b1afe843
Aaron <aaron.perrin@gmail.com>
2019-12-10 07:28:48 -0500

    reduce image size

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "podcast_brand.png" }) {
+      placeholderImage: file(relativePath: { eq: "podcast_brand_1200.png" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
